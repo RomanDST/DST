3924d4d0dffc261a1d0518524026e21aa932edf1
rozerov <rozerov@ccpace.com>
2015-12-27 13:30:38 -0500

    Course Project 2

---
+++ plot3.R
@@ -0,0 +1,19 @@
+NEI <- readRDS("Data/summarySCC_PM25.rds") 
+SCC <- readRDS("Data/Source_Classification_Code.rds") 
+
+NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ] 
+MD <- subset(NEI, fips == 24510) 
+MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008')) 
+
+png(filename='plot3.png', width=800, height=500, units='px') 
+
+install.packages("ggplot2") 
+library(ggplot2)
+
+ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) + 
+  geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') + 
+  ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Year') +  
+  ggtitle('Emissions per Type in Baltimore City, Maryland') + 
+  geom_jitter(alpha=0.10) 
+
+dev.off() 
---
+++ plot5.R
@@ -0,0 +1,19 @@
+NEI <- readRDS("Data/summarySCC_PM25.rds") 
+SCC <- readRDS("Data/Source_Classification_Code.rds") 
+
+NEI$year <- factor(NEI$year, levels=c('1999', '2002', '2005', '2008')) 
+MD.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD') 
+MD.df <- aggregate(MD.onroad[, 'Emissions'], by=list(MD.onroad$year), sum) 
+colnames(MD.df) <- c('year', 'Emissions') 
+
+png(filename='plot5.png') 
+
+install.packages("ggplot2") 
+library(ggplot2)
+
+ggplot(data=MD.df, aes(x=year, y=Emissions)) + guides(fill=F) +  
+  ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') +  
+  ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') +  
+  geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=2)) 
+
+dev.off() 
